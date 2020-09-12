8eb65e0d61f223b349fb68f4dc75cb73b34bebdf
Jessica <jmckinney48045@gmail.com>
2020-09-12 15:57:22 -0400

    migrations added

--- BarkBuddies/BarkBuddies/Models/Pet.cs
+++ BarkBuddies/BarkBuddies/Models/Pet.cs
@@ -1,9 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
 
 namespace BarkBuddies.Models
 {
     public class Pet
     {
+        [Key]
         public int PetId { get; set; }
         public string Name { get; set; }
         public Age Age { get; set; }
